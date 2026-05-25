471f1c64e54bae0bea1e8bef70fab478844b6401
Matheus Manso <matheus.delvalle@outlook.com>
2026-05-25 11:55:37 -0300

    Implementei o script multilínguas

--- hello.py
+++ hello.py
@@ -1,3 +1,27 @@
-print('Tudo o que eu quero, sério, é esse mistério.')
-print('Todo domínio principal é um domínio de fatoração única')
-print(1 + 2 + 3 + 4 + 5 + 6)
+"""
+Este é um hello world multilínguas. Dependendo do idioma, ele imprime hello world no idioma do computador
+"""
+
+__version__ = "0.0.1"
+__author__ = "Matheus"
+__license__ = "Unlicense"
+
+#Dunder = Double underline.
+
+import os
+
+if __name__ == "__main__":
+    current_language = os.getenv("LANG","en_US")[:5]
+    #snake case = padrão de nomenclatura onde as palavras são separadas por underline e todas as letras são minúsculas. Ex: current_language, hello_world, etc.
+    msg = "Hello world!"
+
+    if current_language == "de_DE":
+        msg = "Hallo Welt!"
+    elif current_language == "it_IT":
+        msg = "Ciao, mondo!"
+    elif current_language == "pt_BR":
+        msg = "Olá, mundo!"
+
+    print(msg)
+
+    
